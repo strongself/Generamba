87aaac911d03fd3940627dfa2b41bcef401d9ff4
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-09-20 21:01:27 +0300

    Added a comment with steps required to create a new VIPER module

--- lib/file_generator.rb
+++ lib/file_generator.rb
@@ -8,6 +8,12 @@ class Generamba::FileGenerator
 	end
 
 	def createModule
+		# 1. Create a new model object (name, description, file_path, group_path)
+		# 2. Load a proper Rambafile with config
+		# 3. Generate a scope hashtable from Rambafile and model object
+		# 4. Create new files
+		# 5. Add files to a given xcodeproj.file (maybe take the path from Rambafile)
+
 		template = Tilt.new(File.dirname(__FILE__) + '/templates/assembly.m.liquid')
 
 		module_info = {}
