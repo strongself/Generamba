ef338c4e221ff5526cd0fa06a2be0e4e7d511d3f
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-11-04 19:22:37 +0300

    Implemented copying template to the project dir

--- generamba/lib/generamba/template/installer/installer.rb
+++ generamba/lib/generamba/template/installer/installer.rb
@@ -4,7 +4,14 @@ module Generamba
       full_path = Pathname.new(local_path)
                       .join(template_name + '.rambaspec')
       rambaspec_exist = File.file?(full_path)
-      
+
+      if rambaspec_exist
+        puts('The local path is right! Installing template to the current project directory...')
+        FileUtils.mkdir_p template_name
+        FileUtils.copy_entry(local_path, Pathname.new(template_name))
+        puts("Installing is finished! Now you can call: generamba gen #{template_name}")
+      end
+
     end
   end
 end
