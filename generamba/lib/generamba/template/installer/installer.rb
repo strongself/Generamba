e6bec436341753afad8e488af4e64e484fbd107a
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-11-04 20:29:51 +0300

    Added .rambaspec validity check

--- generamba/lib/generamba/template/installer/installer.rb
+++ generamba/lib/generamba/template/installer/installer.rb
@@ -5,13 +5,21 @@ module Generamba
     def install_local_template(template_name, local_path)
       rambaspec_exist = Generamba::RambaspecValidator.validate_spec_existance(template_name, local_path)
 
-      if rambaspec_exist
-        puts('The local path is right! Installing template to the current project directory...')
-        FileUtils.mkdir_p template_name
-        FileUtils.copy_entry(local_path, Pathname.new(template_name))
-        puts("Installing is finished! Now you can call: generamba gen #{template_name}")
+      if rambaspec_exist == false
+        error_description = "Cannot find #{template_name + '.rambaspec'} in the specified directory. Try another path or name."
+        raise StandardError.new(error_description)
       end
 
+      rambaspec_valid = Generamba::RambaspecValidator.validate_spec(template_name, local_path)
+      if rambaspec_valid == false
+        error_description = "#{template_name + '.rambaspec'} is not valid."
+        raise StandardError.new(error_description)
+      end
+
+      puts('The local path is right! Installing template to the current project directory...')
+      FileUtils.mkdir_p template_name
+      FileUtils.copy_entry(local_path, Pathname.new(template_name))
+      puts("Installing is finished! Now you can call: generamba gen #{template_name}")
     end
   end
 end
--- generamba/lib/generamba/template/installer/rambaspec_validator.rb
+++ generamba/lib/generamba/template/installer/rambaspec_validator.rb
@@ -1,3 +1,5 @@
+require 'settingslogic'
+
 module Generamba
 
   # Provides methods that validate .rambaspec file existance and structure
@@ -10,18 +12,24 @@ module Generamba
     #
     # @return [Bool]
     def self.validate_spec_existance(template_name, template_path)
+      local_spec_path = self.obtain_spec_path(template_name, template_path)
+      rambaspec_exist = File.file?(local_spec_path)
+    end
+
+    def self.validate_spec(template_name, template_path)
+      spec_path = self.obtain_spec_path(template_name, template_path)
+      spec = Settingslogic.new(spec_path)
+
+      is_spec_valid = spec.name != nil && spec.author != nil && spec.version != nil && spec.code_files != nil
+      return is_spec_valid
+    end
+
+    private
+
+    def self.obtain_spec_path(template_name, template_path)
       spec_filename = template_name + '.rambaspec'
       full_path = Pathname.new(template_path)
                       .join(spec_filename)
-      rambaspec_exist = File.file?(full_path)
-
-      if rambaspec_exist == false
-        error_description = "Cannot find #{spec_filename} in the specified directory. Try another path or name."
-        puts(error_description)
-        raise StandardError.new(error_description)
-      end
-
-      return rambaspec_exist
     end
   end
 end
