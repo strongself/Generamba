bd4045aa276d60e0986850b9ad772762724828cd
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-11-08 15:30:51 +0300

    Changed method naming in thor_extension.rb

--- generamba/lib/generamba/cli/setup_command.rb
+++ generamba/lib/generamba/cli/setup_command.rb
@@ -16,14 +16,14 @@ module Generamba::CLI
       if git_username != nil && yes?("Your name in git is configured as #{git_username}. Do you want to use it in code headers? (yes/no)")
         properties['author_name'] = git_username
       else
-        properties['author_name'] = askNonEmptyString('The author name which will be used in the headers:','User name should not be empty')
+        properties['author_name'] = ask_non_empty_string('The author name which will be used in the headers:','User name should not be empty')
       end
 
       properties['author_company'] = ask('The company name which will be used in the headers:')
 
       project_name = Pathname.new(Dir.getwd).basename.to_s
       is_right_project_name = yes?("The name of your project is #{project_name}. Do you want to use it? (yes/no)")
-      properties['project_name'] = is_right_project_name ? project_name : askNonEmptyString('The project name:','Project name should not be empty')
+      properties['project_name'] = is_right_project_name ? project_name : ask_non_empty_string('The project name:','Project name should not be empty')
       properties['prefix']  = ask('The project prefix (if any):')
 
       project_files = Dir['*.xcodeproj']
@@ -41,8 +41,8 @@ module Generamba::CLI
       targets_prompt = ''
       project.targets.each_with_index { |element, i| targets_prompt += ("#{i}. #{element.name}" + "\n") }
 
-      project_target = askIndex("Select the appropriate target for adding your MODULES (type the index):\n" + targets_prompt,project.targets)
-      test_target = askIndex("Select the appropriate target for adding your TESTS (type the index):\n" + targets_prompt,project.targets)
+      project_target = ask_index("Select the appropriate target for adding your MODULES (type the index):\n" + targets_prompt,project.targets)
+      test_target = ask_index("Select the appropriate target for adding your TESTS (type the index):\n" + targets_prompt,project.targets)
 
       project_group_path = ask('The default group path for creating new modules:')
       project_file_path = ask('The default file path for creating new modules:')
--- generamba/lib/generamba/cli/thor_extension.rb
+++ generamba/lib/generamba/cli/thor_extension.rb
@@ -3,20 +3,20 @@ require 'thor'
 module Generamba::CLI
   class ::Thor
     no_commands do
-      def askIndex(message,array)
-        valueIndex = askWithValidation(message,->(value){ (value.to_i >= 0 and value.to_i < array.count) },"Invalid selection. Please enter number from 0 to #{array.count-1}")
-        return array[valueIndex.to_i]
+      def ask_index(message, array)
+        value_index = ask_with_validation(message,->(value){ (value.to_i >= 0 and value.to_i < array.count) },"Invalid selection. Please enter number from 0 to #{array.count-1}")
+        return array[value_index.to_i]
       end
 
-      def askNonEmptyString(message,description='Value should be nonempty string')
-        return askWithValidation(message,->(value){value.length > 0 },description)
+      def ask_non_empty_string(message, description = 'Value should be nonempty string')
+        return ask_with_validation(message,->(value){value.length > 0 },description)
       end
 
-      def askWithValidation(message,isValidValue,description='Invalid value')
+      def ask_with_validation(message, is_valid_value, description = 'Invalid value')
         loop do
           value = ask(message)
           puts("value from input #{value}")
-          break if isValidValue.call(value)
+          break if is_valid_value.call(value)
           puts(description)
         end
       end
