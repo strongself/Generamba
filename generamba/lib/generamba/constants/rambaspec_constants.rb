3cde309943acf10674daef74201030b3d0114306
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-11-14 22:28:05 +0300

    Added new cli command: generamba template create

--- generamba/lib/generamba/constants/rambaspec_constants.rb
+++ generamba/lib/generamba/constants/rambaspec_constants.rb
@@ -2,9 +2,15 @@ module Generamba
 
   # Keys of .rambaspec files
   TEMPLATE_NAME_KEY = 'name'
+  TEMPLATE_SUMMARY_KEY = 'summary'
+  TEMPLATE_AUTHOR_KEY = 'author'
+  TEMPLATE_VERSION_KEY = 'version'
+  TEMPLATE_LICENSE_KEY = 'license'
+
   TEMPLATE_CODE_FILES_KEY = 'code_files'
   TEMPLATE_TEST_FILES_KEY = 'test_files'
-
   TEMPLATE_FILE_NAME_KEY = 'name'
   TEMPLATE_FILE_PATH_KEY = 'path'
+
+  TEMPLATE_DEPENDENCIES_KEY = 'dependencies'
 end
