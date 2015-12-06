0744a511f85b139fe83f5fdfe4a3c130550980fd
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-12-06 15:36:30 +0300

    Fixed #39: generamba template install now produces more informative error message when working directory is empty

--- generamba/lib/generamba/cli/template/template_install_command.rb
+++ generamba/lib/generamba/cli/template/template_install_command.rb
@@ -3,6 +3,13 @@ module Generamba::CLI
 
     desc 'install', 'Installs all the templates specified in the Rambafile from the current directory'
     def install
+      does_rambafile_exist = Dir[RAMBAFILE_NAME].count > 0
+
+      if (does_rambafile_exist == false)
+        puts('Rambafile not found! Run `generamba setup` in the working directory instead!'.red)
+        return
+      end
+
       template_processor = Generamba::TemplateProcessor.new
       template_processor.install_templates
     end
