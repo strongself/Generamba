7498404cbd6ca2efbf4cbe8bdfc7a111c27bef95
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-12-13 14:12:02 +0300

    Added `generamba search` command

--- generamba/lib/generamba/cli/template/template_group.rb
+++ generamba/lib/generamba/cli/template/template_group.rb
@@ -1,6 +1,7 @@
 require 'generamba/cli/template/template_install_command.rb'
 require 'generamba/cli/template/template_create_command.rb'
 require 'generamba/cli/template/template_list_command.rb'
+require 'generamba/cli/template/template_search_command.rb'
 
 module Generamba::CLI
   class Application < Thor
