4aa56e539c736375d464c7af0d0fe896a91c02e6
Herman Saprykin <g.saprykin@rambler-co.ru>
2016-01-08 00:13:39 +0300

    Fixed checking extensions of files for adding to targets.

--- generamba/spec/generamba_spec.rb
+++ generamba/spec/generamba_spec.rb
@@ -5,7 +5,4 @@ describe Generamba do
     expect(Generamba::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
---
+++ generamba/spec/xcodeprojhelper_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+
+describe 'XcodeprojHelper' do
+    describe 'method add_file_to_target?' do
+      it 'should return true for Swift file' do
+        filename = 'Test.swift'
+        result = Generamba::XcodeprojHelper::add_file_to_target?(filename)
+        expect(result).to eq(true)
+      end
+
+      it 'should return true for Obj-C file' do
+        filename = 'Test.m'
+        result = Generamba::XcodeprojHelper::add_file_to_target?(filename)
+        expect(result).to eq(true)
+      end
+
+      it 'should return true for C++ file' do
+        filename = 'Test.mm'
+        result = Generamba::XcodeprojHelper::add_file_to_target?(filename)
+        expect(result).to eq(true)
+      end
+    end
+end
