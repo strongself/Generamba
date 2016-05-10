51dbb752316edcdd12190bb243da630d12a208e6
Aleksey Garbarev <alex.garbarev@gmail.com>
2016-05-10 22:55:57 +0600

    indent fixed

--- lib/generamba/code_generation/content_generator.rb
+++ lib/generamba/code_generation/content_generator.rb
@@ -16,9 +16,9 @@ module Generamba
 			Liquid::Template.file_system = Liquid::LocalFileSystem.new(template.template_path.join('snippets'), '%s.liquid')
 
 			template = Liquid::Template.parse(file_source)
-            filename_template = self.file_name_template(file)
+			filename_template = self.file_name_template(file)
       
-            file_basename = File.basename(file[TEMPLATE_FILE_NAME_KEY])
+			file_basename = File.basename(file[TEMPLATE_FILE_NAME_KEY])
 
 			module_info = {
 					'name' => code_module.name,
@@ -41,22 +41,22 @@ module Generamba
 					'prefix' => code_module.prefix
 			}
       
-            module_info['file_basename'] = file_basename
+			module_info['file_basename'] = file_basename
 
-            file_name = filename_template.render(scope)
+			file_name = filename_template.render(scope)
 
-            module_info['file_name'] = file_name
-            module_info.delete('file_basename')
+			module_info['file_name'] = file_name
+			module_info.delete('file_basename')
 
 			content = template.render(scope)
 
 			return file_name, content
 		end
     
-        def self.file_name_template(file)
-            template_default_text = "{{ prefix }}{{ module_info.name }}{{ module_info.file_basename }}"
-            template_text = file[TEMPLATE_FLIE_FILENAME_KEY] || template_default_text
-            return Liquid::Template.parse(template_text) 
-        end
+		def self.file_name_template(file)
+			template_default_text = "{{ prefix }}{{ module_info.name }}{{ module_info.file_basename }}"
+			template_text = file[TEMPLATE_FLIE_FILENAME_KEY] || template_default_text
+			return Liquid::Template.parse(template_text)
+		end
 	end
 end
