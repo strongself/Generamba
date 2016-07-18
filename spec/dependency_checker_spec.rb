23f82d3a5c4f19f65282cd84b6d4025bd0786cd5
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-07-18 23:16:02 +0300

    Added a simple method which checks the presence of dependency in the Cartfile

--- spec/dependency_checker_spec.rb
+++ spec/dependency_checker_spec.rb
@@ -27,4 +27,36 @@ describe 'DependencyChecker' do
     end
   end
 
+  describe 'method check_all_required_dependencies_has_in_cartfile' do
+    it 'should do nothing' do
+      dependencies = ['ViperMcFlurry']
+      cartfile_path = 'Cartfile'
+
+      expect(STDOUT).not_to receive(:puts).with("[Warning] Dependencies #{dependencies} missed in Cartfile".yellow)
+
+      @checker.check_all_required_dependencies_has_in_cartfile(dependencies, nil)
+      @checker.check_all_required_dependencies_has_in_cartfile(nil, cartfile_path)
+    end
+
+    it 'should show warning message if dependency missing' do
+      dependencies = ['ViperMcFlurry']
+      cartfile_path = 'Cartfile'
+
+      allow(File).to receive(:read).and_return('Typhoon')
+      expect(STDOUT).to receive(:puts).with("[Warning] Dependencies #{dependencies} missed in Cartfile".yellow)
+
+      @checker.check_all_required_dependencies_has_in_cartfile(dependencies, cartfile_path)
+    end
+
+    it 'should not show warning message if dependency is in place' do
+      dependencies = ['ViperMcFlurry']
+      cartfile_path = 'Cartfile'
+
+      allow(File).to receive(:read).and_return('github "Rambler-iOS/ViperMcFlurry"')
+      expect(STDOUT).not_to receive(:puts).with("[Warning] Dependencies #{dependencies} missed in Cartfile".yellow)
+
+      @checker.check_all_required_dependencies_has_in_cartfile(dependencies, cartfile_path)
+    end
+  end
+
 end
