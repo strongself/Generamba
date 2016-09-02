946c1588e033e1f0f3a2e9d858c6a7fb1ad57dfb
Beniamin Sarkisyan <b.sarkisyan@rambler-co.ru>
2016-09-03 02:46:38 +0300

    [RESOLVED] Added ability use custom parameters in rambaspec #128

--- lib/generamba/code_generation/module_template.rb
+++ lib/generamba/code_generation/module_template.rb
@@ -6,10 +6,18 @@ module Generamba
   class ModuleTemplate
     attr_reader :template_name, :template_path, :code_files, :test_files, :dependencies
 
-    def initialize(name)
+    def initialize(name, options = nil)
       spec_path = TemplateHelper.obtain_spec(name)
-      spec = YAML.load_file(spec_path)
-      
+
+      unless options
+        spec = YAML.load_file(spec_path)
+      else
+        spec_source = IO.read(spec_path)
+        spec_template = Liquid::Template.parse(spec_source)
+        spec_content = spec_template.render(options)
+        spec = YAML.load(spec_content)
+      end
+
       @code_files = spec[TEMPLATE_CODE_FILES_KEY]
       @test_files = spec[TEMPLATE_TEST_FILES_KEY]
       @template_name = spec[TEMPLATE_NAME_KEY]
--- lib/generamba/module_generator.rb
+++ lib/generamba/module_generator.rb
@@ -1,6 +1,7 @@
 require 'fileutils'
 
 require 'generamba/helpers/xcodeproj_helper.rb'
+require 'generamba/helpers/module_info_generator.rb'
 
 module Generamba
 
@@ -51,8 +52,8 @@ module Generamba
 			puts "Name: #{name}".green
 			puts "Module file path: #{code_module.module_file_path}".green
 			puts "Module group path: #{code_module.module_group_path}".green
-			puts !code_module.test_file_path ? '' : "Test file path: #{code_module.test_file_path}".green
-			puts !code_module.test_group_path ? '' : "Test group path: #{code_module.test_group_path}".green
+			puts "Test file path: #{code_module.test_file_path}".green if code_module.test_file_path
+			puts "Test group path: #{code_module.test_group_path}".green if code_module.test_group_path
 		end
 
 		def process_files_if_needed(files, name, code_module, template, project, targets, group_path, dir_path)
@@ -76,8 +77,10 @@ module Generamba
 
 				file_group = File.dirname(file[TEMPLATE_NAME_KEY])
 
+				module_info = ModuleInfoGenerator.new(code_module)
+
 				# Generating the content of the code file and it's name
-				file_name, file_content = ContentGenerator.create_file(file, code_module, template)
+				file_name, file_content = ContentGenerator.create_file(file, module_info.scope, template)
 				file_path = dir_path.join(file_group).join(file_name)
 
 				# Creating the file in the filesystem
