e20056af270aa8fe8378b970e4a46bd332480742
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-09-27 15:26:21 +0300

    Increased version to 1.2.2

--- lib/generamba/version.rb
+++ lib/generamba/version.rb
@@ -1,5 +1,5 @@
 module Generamba
-  VERSION = '1.2.1'
-  RELEASE_DATE = '26.09.2016'
+  VERSION = '1.2.2'
+  RELEASE_DATE = '27.09.2016'
   RELEASE_LINK = "https://github.com/rambler-ios/Generamba/releases/tag/#{VERSION}"
 end
