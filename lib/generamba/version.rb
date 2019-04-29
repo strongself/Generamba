b8d4d88efc2931a2129e92b4e2b2da312da6b5ae
Сычев Александр Александрович <Sychev.A.Aleksandr@sberbank.ru>
2019-04-29 12:37:33 +0300

    [add] new version

--- lib/generamba/version.rb
+++ lib/generamba/version.rb
@@ -1,5 +1,5 @@
 module Generamba
-  VERSION = '1.4.1'
-  RELEASE_DATE = '29.08.2017'
+  VERSION = '1.5.0'
+  RELEASE_DATE = '29.04.2019'
   RELEASE_LINK = "https://github.com/rambler-digital-solutions/Generamba/releases/tag/#{VERSION}"
 end
